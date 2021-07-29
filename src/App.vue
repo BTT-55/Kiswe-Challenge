d8de82f6731f8010b66e72ab1b1f09ae798f2c79
BTT-55 <67373871+BTT-55@users.noreply.github.com>
2021-07-29 21:28:26 +0200

    Basic layout
    Basic layout for the login & register pages

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <!-- zou hierboven nog een headerbar komen -->
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+  #nav {
+    margin-top: 25px;
+    margin-bottom: 30px;
+    background-color: #ffffff;
+    text-align: right;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #d88d00;
+      }
+    }  
+  } 
+
+</style>
